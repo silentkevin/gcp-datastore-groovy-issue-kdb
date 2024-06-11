92681a4f9596e8120ff930e3d2ec7e0158df27ce
Kevin Brown <kevin.brown@scopely.com>
2024-06-11 14:25:27 -0400

    initial

---
+++ src/test/groovy/com/silvertree/gcpdatastoregroovyissuekdb/GcpDatastoreGroovyIssueKdbApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.silvertree.gcpdatastoregroovyissuekdb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GcpDatastoreGroovyIssueKdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
